73e0f901de9076bc9b0a25b253d7ec28c767a14c
michael chanam <hanammichael@gmail.com>
2026-02-28 19:03:43 +0200

    tiny fixes

--- scripts/player/states/grounded/IdleState.gd
+++ scripts/player/states/grounded/IdleState.gd
@@ -5,8 +5,8 @@ func enter(player):
 	player.animator.play(PlayerAnimator.Animations.IDLE)
 
 func _grounded_update(player, state_machine, delta):
-	player.movement.stop_moving(player)
-	
 	if (player.input.get_direction()):
 		state_machine.change_state(PlayerStateMachine.StateID.MOVE)
 		return
+
+	player.movement.stop_moving(player)
